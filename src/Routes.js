a8ce4b8da7cf79bd6d29b2e2a344bdd0e1b61547
ridou99@gmail.com <ridou99@gmail.com>
2020-07-27 14:40:30 +0900

    [송수호] 깃허브 로그인 컨플릭트 해결중

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,6 @@ import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Login/Signup";
 import Nav from "./Pages/Main/Nav";
 import Mapcomponent from "./Pages/Productmap/Mapcomponent";
-import Underbar from "./Pages/Underbar/Underbar";
 
 class Routes extends React.Component {
   render() {
@@ -17,14 +16,9 @@ class Routes extends React.Component {
           <Route exact path="/Signup" component={Signup} />
           <Route exact path="/Nav" component={Nav} />
           <Route exact path="/Mapcomponent" component={Mapcomponent} />
-          <Route exact path="/Underbar" component={Underbar} />
         </Switch>
       </Router>
     );
   }
 }
 export default Routes;
-
-
-
-
