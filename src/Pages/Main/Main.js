007db66b3bd2de53db5361453c0febe3b8dbeb0f
ridou99@gmail.com <ridou99@gmail.com>
2020-07-27 15:35:43 +0900

    [송수호]fix: 컨플릭트 에러 수정중

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,30 @@
 import React from "react";
+import CitrusFeed from "./CitrusFeed";
+import ItemSlide from "./ItemSlide";
+import Appointment from "./Appointment";
+import Store from "./Store";
+import Quote from "./Quote";
+import Footer from "./Footer";
+import Login from "../Login/Login";
+import Signup from "../Login/Signup";
+// import Mapcomponent from "./Mapcomponent";
+import "./Main.scss";
 
 class Main extends React.Component {
   render() {
-    return <div className="Main">df</div>;
+    return (
+      <div className="Main">
+        <CitrusFeed />
+        <ItemSlide />
+        <Appointment />
+        <Store />
+        <Quote />
+        <Footer />
+        <Signup />
+        <Login />
+        {/* <Mapcomponent /> */}
+      </div>
+    );
   }
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main/Main";
+import Login from "./Pages/Login/Login";
+import Sign from "./Pages/Login/Signup";
 import ProductList from "./Pages/ProductList/ProductList";
 import Productdetails from "./Pages/Product/Productdetails";
 
@@ -10,6 +12,8 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route exact path="/Login" component={Login} />
+          <Route exact path="/Signup" component={Sign} />
           <Route exact path="/skin" component={ProductList} />
           <Route exact path="/skin/details" component={Productdetails} />
         </Switch>
