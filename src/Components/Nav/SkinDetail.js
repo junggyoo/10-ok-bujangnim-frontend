1ac03639479132e5785c2c850b4b8557ac3ab385
ridou99@gmail.com <ridou99@gmail.com>
2020-07-31 11:07:36 +0900

    [송수호]modify:상세페이지 1개의 카테고리 접근경로 수정완료

--- src/Components/Nav/SkinDetail.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./SkinDetail.scss";
-
-class SkinDetail extends React.Component {
-  render() {
-    return <div className="SkinDetail"></div>;
-  }
-}
-export default SkinDetail;
